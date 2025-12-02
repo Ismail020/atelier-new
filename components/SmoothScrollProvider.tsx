06f96b5b8f777371a43ff7b01d33cfc5a966498e
Ismail Çetin <isocetin1907@gmail.com>
2025-12-02 17:23:49 +0100

    feat: Refactor SmoothScrollProvider to support children and implement scroll-to-top on route change

--- components/SmoothScrollProvider.tsx
+++ components/SmoothScrollProvider.tsx
@@ -3,9 +3,11 @@
 import gsap from "gsap";
 import { LenisRef, ReactLenis } from "lenis/react";
 import { useEffect, useRef } from "react";
+import { usePathname } from "next/navigation";
 
-export default function SmoothScrollProvider() {
+export default function SmoothScrollProvider({ children }: { children: React.ReactNode }) {
   const lenisRef = useRef<LenisRef>(null);
+  const pathname = usePathname();
 
   useEffect(() => {
     function update(time: number) {
@@ -17,5 +19,18 @@ export default function SmoothScrollProvider() {
     return () => gsap.ticker.remove(update);
   }, []);
 
-  return <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />;
+  // Scroll to top on route change
+  useEffect(() => {
+    if (lenisRef.current?.lenis) {
+      lenisRef.current.lenis.scrollTo(0, { immediate: true });
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname]);
+
+  return (
+    <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
+      {children}
+    </ReactLenis>
+  );
 }
